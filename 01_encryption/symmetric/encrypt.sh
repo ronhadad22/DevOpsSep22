454a31190e50f1ec6160042f0e0c858f9eb167d5
Dean Sorie <66969624+deanosaurx@users.noreply.github.com>
2022-10-30 20:09:16 +0200

    Delete 01_encryption directory

--- 01_encryption/symmetric/encrypt.sh
+++
@@ -1,7 +0,0 @@
-# -e              Encrypt file
-# -aes-256-cbc    The algo name
-# -k              The key
-# -in             The input file
-# -out            The output file
-
-openssl enc -e -aes-256-cbc -pbkdf2 -k myKey -in secret -out encrypted_secret
