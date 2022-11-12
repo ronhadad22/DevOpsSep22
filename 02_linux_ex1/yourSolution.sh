daeaab18c09fac21e2aff4f0384b43c11fec6a5a
shaniben1 <shaniben5253@gmail.com>
2022-11-12 20:27:29 +0200

    shani's solution

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,1 +1,13 @@
-devops+"great"
+#!/bin/bash
+
+
+#wget https://devops-course-sep-22.s3.sa-east-1.amazonaws.com/ex1/secretGenerator.tar.gz
+#tar xvzf secretGenerator.tar.gz
+#cd src
+#chmod u+x generateSecret.sh
+#rm -rf maliciousFiles
+#mkdir secretDir
+#touch secretDir/.secret
+#chmod 600 secretDir/.secret
+#rm important.link #or unlink important.link
+#./generateSecret.sh
