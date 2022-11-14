9dc1c7fef6adca2dcf5fe470da7c7b7922268b23
Shay Abudi <shayabud8@gmail.com>
2022-11-14 07:56:48 +0200

    Solution

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,5 +1,10 @@
-mkdir blabla
-touch blabla
-...
-rm blabla
-...
+tar -xzvf secretGenerator.tar.gz
+cd src
+chmod +x generateSecret.sh
+mkdir secretDir
+rm -rf maliciousFiles
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+unlink important.link
+./generateSecret.sh
+cat ./secretDir/.secret
