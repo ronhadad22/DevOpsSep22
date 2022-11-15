423be630162ed055f4f685df79f08039605a35be
Ben <benportmantech@gmail.com>
2022-11-15 21:58:04 +0200

    Ben's Solution

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,1 +1,10 @@
-devops+"great"
+wget https://devops-course-sep-22.s3.sa-east-1.amazonaws.com/ex1/secretGenerator.tar.gz
+tar -xf secretGenerator.tar.gz
+cd src
+mkdir secretDir
+rm -rf maliciousFiles
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+unlink important.link
+/bin/bash generateSecret.sh
+cat ./secretDir/.secret
