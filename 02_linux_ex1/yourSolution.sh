bd2c325fa079ff1fb0dd4ade7e50c61ea18ac826
daria <dariakalugny2405@gmail.com>
2022-11-16 22:36:57 +0200

    daria

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -6,23 +6,18 @@ chmod +x yourSolution.sh
 if [ ! -d "secretDir" ]; then
 
  mkdir "secertDir"
-   exit 1
 fi
 
 # practice dir deletion and file move
 if [ -d "maliciousFiles" ]; then
 
- rm amIMaliciousOrNot.whoKnows
- rm someFileIsLinkingToMe.BeAware
  rm -r maliciousFiles
-    exit 1
 fi
 
 # practice file creation
 if [ ! -f "secretDir/.secret" ]; then
 
  touch .secert
-   exit 1
 fi
 
 # practice change permissions
@@ -30,14 +25,12 @@ OCTAL_PERMISSIONS=$(stat -c "%a" secretDir/.secret)
 if [ "$OCTAL_PERMISSIONS" != "600" ]; then
 
 chmod 600 .secret
-    exit 1
 fi
 
 # practice file linking understanding
 if [ -L 'important.link' ] && [ ! -e 'important.link' ]; then
 
   rm -r important.link
-    exit 1
 fi
 
 cat ./CONTENT_TO_HASH | xargs | md5sum > secretDir/.secret&& echo "Done! Your secret was stored in secretDir/.secret"
