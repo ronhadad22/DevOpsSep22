469101bc0942a203ad40366fdebe77bcd927b708
mynameisarkadi <mynameisarkadi@hotmail.com>
2022-11-20 01:44:09 +0200

    Arkadi's solution

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,1 +1,15 @@
-devops+"great"
+cd src
+
+mkdir secretDir
+
+rm -rf maliciousFiles
+
+touch secretDir/.secret
+
+chmod 600 secretDir/.secret
+
+unlink important.link
+
+/bin/bash generateSecret.sh
+
+cat ./secretDir/.secret
