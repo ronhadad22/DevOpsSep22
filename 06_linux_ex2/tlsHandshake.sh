6058b2baf07bcf313b67763fea217a10bbda9d78
deanosaurx <deansorie@icloud.com>
2022-11-19 15:08:10 +0200

    FINALLYgit statusgit status! FINISHED!

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-wget https://devops-course-sep-22.s3.sa-east-1.amazonaws.com/ex2/cert-ca-aws.pem
-
 RESPONSE1=$(curl -d '{ "clientVersion": "3.2", "message": "Client Hello" }' -H "Content-Type: application/json" -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/clienthello)
 
 
@@ -11,30 +9,26 @@ VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
 RANDOM_KEY=$(openssl rand -hex 32)
 echo $RANDOM_KEY | dd of=masterKey.txt
 
-echo "RANDOM KEY IS $RANDOM_KEY"
-
-echo " SESSION KEY IS $SESSION_ID"
-
 if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid."
   exit 1
 fi
 
-MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt cert.pem | base64 -w 0)
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
 
-echo "MASTER KEY IS $MASTER_KEY"
-
-RESPONSE2=$(curl -d '{ "sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt me and send to client!"  }' -H "Content-Type: application/json" -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/keyexchange)
-
+RESPONSE2=$(curl -d '{ "sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt me and send to client!"  }' -H "Content-Transfer-Encoding: base64" -H "Content-Type: application/json" -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/keyexchange)
 
 ENCRYPTED_MESSAGE=$(echo $RESPONSE2 | jq -r '.encryptedSampleMessage')
-
-
-echo "ENCRYPTED MESSAGE IS $ENCRYPTED_MESSAGE"
 
 echo $ENCRYPTED_MESSAGE | dd of=encSampleMsg.txt
 
 cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
 
+DECRYPTED_SAMPLE_MESSAGE=$(cat encSampleMsgReady.txt | openssl enc -d -aes-256-cbc -pbkdf2 -k $RANDOM_KEY) 
 
-# Unable to decrypt as of yet, will figure this out tomorrow :\
+if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
+  echo "Server symmetric encryption using the exchanged master-key has failed."
+  exit 1
+else
+  echo "Client-Server TLS handshake has been completed successfully"
+fi
