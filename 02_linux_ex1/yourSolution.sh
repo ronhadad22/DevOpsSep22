2ab7f38759ea7b55743a4cda5a722bfc44504b2e
mortis1983 <mortis1983@gmail.com>
2022-11-19 22:14:42 +0200

    Adding a solution to question three

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,1 +1,15 @@
 devops+"great"
+
+tar -xf secretGenerator.tar.gz
+cd src
+rm -rf maliciousFiles
+rm -rf important.link
+mkdir secretDir
+cd secretDir
+touch .secret
+chmod 600 secretDir/.secret
+/bin/bash generateSecret.sh
+cd secretDir
+cp .secret /home/mortis1983/src
+cd ..
+cp .secret yourSolution.sh
