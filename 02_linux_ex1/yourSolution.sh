0e1b33be5bcf73d68fd98a5083e10f0d8d8613b8
avijw96 <82993109+avijw96@users.noreply.github.com>
2022-11-21 19:16:59 +0200

    Avi Yaaqov solution!!

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,1 +1,10 @@
-devops+"great"
+#!/bin/bash
+
+mkdir secretDir
+rmdir maliciousFiles
+> secretDir/.secret
+sudo chmod 600 secretDir/.secret
+OCTAL_PERMISSIONS=$(stat -c "%a" secretDir/.secret)
+unlink important.link
+at ./CONTENT_TO_HASH | xargs | md5sum > secretDir/.secret && echo "Done! Your secret was stored in secretDir/.secret"
+
