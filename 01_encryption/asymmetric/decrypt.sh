3b23cb72ca6769e4434107ef666396b8fa0b511f
Ben <benportmantech@gmail.com>
2022-12-03 20:12:00 +0200

    Certificate

--- 01_encryption/asymmetric/decrypt.sh
+++
@@ -1,1 +0,0 @@
-openssl rsautl -decrypt -inkey ~/.ssh/id_rsa_pr.pem -in message.encrypted -out mmessage.decrypted
--- 01_encryption/symmetric/decrypt.sh
+++
@@ -1,7 +0,0 @@
-# -e              Encrypt file
-# -aes-256-cbc    The algo name
-# -k              The key
-# -in             The input file
-# -out            The output file
-
-openssl enc -d -aes-256-cbc -pbkdf2 -k myKey -in encrypted_secret -out decrypted_secret
