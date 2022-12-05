fabd25b947faa5ee02e81c3795c6eee57a7fd75c
avibendersky <avi.bendersky@gmail.com>
2022-12-05 22:01:16 +0200

    solution

--- 02_linux_ex1/yourSolution.sh
+++ 02_linux_ex1/yourSolution.sh
@@ -1,1 +1,11 @@
-devops+"great"
+wget https://devops-course-sep-22.s3.sa-east-1.amazonaws.com/ex1/secretGenerator.tar.gz
+tar -xzvf secretGenerator.tar.gz
+cd src/
+chmod +x generateSecret.sh
+mkdir secretDir
+rm -rf maliciousFiles
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+unlink important.link
+./generateSecret.sh
+cat secretDir/.secret
