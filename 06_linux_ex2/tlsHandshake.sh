2576b982b2d73685aaaff42acfc0c89ad5463808
Ben <benportmantech@gmail.com>
2022-12-07 19:58:16 +0200

    Certificate

--- 06_linux_ex2/tlsHandshake.sh
+++ 06_linux_ex2/tlsHandshake.sh
@@ -1,26 +1,37 @@
-1)curl -d '{ "clientVersion": "3.2", "message": "Client Hello" }' -H "Content-Type: application/json" -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/clienthello | tee AWS.txt
+curl -d '{ "clientVersion": "3.2", "message": "Client Hello" }' -H "Content-Type: application/json" -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/clienthello | tee AWS.txt
 
-2)SESSION_ID=$(jq -r '.sessionID' AWS.txt)
+SESSION_ID=$(jq -r '.sessionID' AWS.txt)
 
-3)jq -r '.serverCert' AWS.txt | tee cert.pem
+jq -r '.serverCert' AWS.txt | tee cert.pem
 
-4)wget https://devops-course-sep-22.s3.sa-east-1.amazonaws.com/ex2/cert-ca-aws.pem
+wget https://devops-course-sep-22.s3.sa-east-1.amazonaws.com/ex2/cert-ca-aws.pem
 
-5)VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
+VERIFICATION_RESULT=$( openssl verify -CAfile cert-ca-aws.pem cert.pem )
 
-6) if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
+if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
   echo "Server Certificate is invalid."
   exit 1
 fi
 
-7) echo $RANDOM | md5sum | head -c 32 | tee masterKey.txt
+RANDOM_KEY=$(openssl rand -hex 32)
 
-8) MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
+echo $RANDOM_KEY | dd of=masterKey.txt
 
-9) curl -d '{"sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' -H "Content-Type: application/json" -H "Content-Transfer-Encoding: base64" -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/keyexchange | tee encSampleMsg.txt
+MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
 
-10) jq -r '.encryptedSampleMessage' encSampleMsg.txt | base64 -d | tee encSampleMsgReady.txt
+RESPONSE2=$(curl -d '{ "sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt me and send to client!"  }' -H "Content-Transfer-Encoding: base64" -H "Content-Type: application/json" -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/keyexchange)
 
-11) MASTER=$(cat masterKey.txt)
+ENCRYPTED_MESSAGE=$(echo $RESPONSE2 | jq -r '.encryptedSampleMessage')
 
-12) openssl enc -d -aes-256-cbc -pbkdf2 -k $MASTER -in encSampleMsgReady.txt -out decrypted.txt
+echo $ENCRYPTED_MESSAGE | dd of=encSampleMsg.txt
+
+cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
+
+DECRYPTED_SAMPLE_MESSAGE=$(cat encSampleMsgReady.txt | openssl enc -d -aes-256-cbc -pbkdf2 -k $RANDOM_KEY)
+
+if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
+  echo "Server symmetric encryption using the exchanged master-key has failed."
+  exit 1
+else
+  echo "Client-Server TLS handshake has been completed successfully"
+fi
