848e8895c2f4e64451221feacbecceafd8cbbcfa
avijw96 <82993109+avijw96@users.noreply.github.com>
2022-12-10 12:02:22 +0200

    Avi Yaaqov solution!!

---
+++ 06_linux_ex2/tlsHandshake.sh
@@ -0,0 +1,29 @@
+##aviyaaqov answear
+
+curl  -X POST \http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/clienthello \ -H 'Content-Type: application/json' \-d '{"clientVersion": "3.2", "message": "Client Hello"}' | jq '.' > response.json
+ cat response.json | jq -r '.serverCert' > cert.pem
+wget -q https://devops-course-sep-22.s3.sa-east-1.amazonaws.com/ex2/cert-ca-aws.pem
+VERIFICATION_RESULT=$(openssl verify -CAfile cert-ca-aws.pem cert.pem)
+
+    if [ "$VERIFICATION_RESULT" != "cert.pem: OK" ]; then
+       echo "Server Certificate is invalid."
+      exit 1
+fi
+        openssl rand -hex 32  > masterKey.txt
+        RANDOM_KEY=$(cat masterKey.txt)
+        MASTER_KEY=$(openssl smime -encrypt -aes-256-cbc -in masterKey.txt -outform DER cert.pem | base64 -w 0)
+
+        curl -sb -X POST http://ec2-54-207-102-47.sa-east-1.compute.amazonaws.com:8081/keyexchange \
+        -H "Content-Transfer-Encoding: base64" -H "Content-Type: application/json" \
+        -d '{"sessionID": "'$SESSION_ID'", "masterKey": "'$MASTER_KEY'", "sampleMessage": "Hi server, please encrypt me and send to client!"}' | jq '.'>ENC_RESP.json
+
+cat ENC_RESP.json | jq -r '.encryptedSampleMessage' > encSampleMsg.txt
+cat encSampleMsg.txt | base64 -d > encSampleMsgReady.txt
+DECRYPTED_SAMPLE_MESSAGE=$(openssl enc -d -aes-256-cbc -pbkdf2 -k $RANDOM_KEY -in encSampleMsgReady.txt)
+
+ if [ "$DECRYPTED_SAMPLE_MESSAGE" != "Hi server, please encrypt me and send to client!" ]; then
+        echo "Server symmetric encryption using the exchanged master-key has failed."
+        exit 1
+    else
+        echo "Client-Server TLS handshake has been completed successfully"
+    fi
