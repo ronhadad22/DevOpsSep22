2fb065aea5e0dcdf59e7f7adc0a068a8c76b578e
ronhadad <ronhadad22@gmail.com>
2022-12-14 14:54:26 +0200

    add test

---
+++ 08_python_class/bank_account.py
@@ -0,0 +1,51 @@
+"""
+Create a bank account class that has two attributes:
+
+* owner
+* balance
+
+and two methods:
+
+* deposit
+* withdraw
+
+As an added requirement, withdrawals may not exceed the available balance.
+Instantiate your class, make several deposits and withdrawals, and test to make sure the account can't be overdrawn.
+"""
+
+
+class Account:
+    pass
+
+if __name__ == '__main__':
+    pass
+
+    # 1. Instantiate the class
+    # joint_account = acct1 + acct2
+
+    # 2. Print the object
+
+    # output:
+    # >> Account owner:   Jose
+    # >> Account balance: $100
+
+    # 3. Show the account owner attribute
+
+    # >> 'Jose'
+
+    # 4. Show the account balance attribute
+
+    # >> 100
+
+    # 5. Make a series of deposits and withdrawals
+
+    # >> Deposit Accepted
+
+
+    # Withdrawal Accepted
+
+    # 6. Make a withdrawal that exceeds the available balance
+
+    # Funds Unavailable!
+
+
