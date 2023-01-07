1909b61ef07efc5df3fa6dfe401e5859edd5c92a
alonb <alonbarda@gmail.com>
2023-01-07 14:36:05 +0200

    alon solution's

--- python_katas/kata_3/test.py
+++ python_katas/kata_3/test.py
@@ -5,6 +5,43 @@ from python_katas.utils import unittest_runner
 
 class TestKnapsack(unittest.TestCase):
     """
+    5 Kata
+    """
+    def test_sample(self):
+        items = {
+        'bed': (43, 10),
+        'iphone': (5, 200),
+        'ring': (2, 150),
+        'chair': (20, 5),
+        'table': (32, 8),
+        'tv': (22, 200),
+        'ring2': (2, 300),
+        'pc': (20, 530)
+        }
+        self.assertEqual(questions.knapsack(items, knapsack_limit=50))
+
+        def test_empty_house(self):
+            items = {}
+            self.assertEqual(questions.knapsack(items, knapsack_limit=50))
+
+            def test_overload(self):
+                items = {
+                    'bed': (52, 10),
+                    'iphone': (51, 200),
+                    'ring': (52, 150),
+                    }
+                self.assertEqual(questions.knapsack(items, knapsack_limit=50))
+
+        def test_hole_items(self):
+            items = {
+                'bed': (1, 10),
+                'iphone': (5, 200),
+                'ring': (2, 150),
+                'chair': (1, 5),
+                'table': (1, 8),
+                }
+            self.assertEqual(questions.knapsack(items, knapsack_limit=50))
+
 
 
         pass
