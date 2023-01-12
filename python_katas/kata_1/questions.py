b1d0ca267b8a66e8411b10216f7247e30f464c03
Rosin1992 <114486997+Rosin1992@users.noreply.github.com>
2023-01-12 19:25:26 +0200

    Update questions.py

--- python_katas/kata_1/questions.py
+++ python_katas/kata_1/questions.py
@@ -1,179 +1,186 @@
 def sum_of_element(elements):
     """
     1 Kata
-
     :param elements: list of integers
     :return: Return int - the sum of all elements.
     """
     s = 0
     for num in elements:
         s = s + num
-
     return s
 
 
 def verbing(word):
     """
     1 Kata
-
     Given a string 'word', if its length is at least 3, add 'ing' to its end.
     Unless it already ends in 'ing', in which case add 'ly' instead.
     If the string length is less than 3, leave it unchanged.
-
     e.g.
     teach -> teaching
     do -> do
     swimming -> swimmingly
-
     :param word: str
     :return: Return the resulting string.
     """
-    return None
+
+    if word.endswith("ing"):
+        return word + "ly"
+
+    if len(word) >= 3:
+        return word + "ing"
+    else:
+        return word
 
 
 def words_concatenation(words):
     """
     1 Kata
-
     Given a list of words, write a program that concatenates the words.
-
     For example:
     words_concatenation(['take', 'me', 'home']) returns 'take me home'
-
     :param words: list of str
     :return: Return the resulting string.
     """
-    return None
+
+    return " ".join(words)
 
 
 def reverse_words_concatenation(words):
     """
     1 Kata
-
     Given a list of words, write a program that concatenates the words in a reverse way
-
     For example:
     reverse_words_concatenation(['take', 'me', 'home']) returns 'home me take'
-
     :param words: list of str
     :return: Return the resulting string.
     """
-    return None
+    words.reverse()
+    return " ".join(words)
 
 
 def is_unique_string(some_str):
     """
     2 Kata
-
     Given a string, the function returns True if all characters in the string are unique, False otherwise
-
     e.g
     'abcd' -> True
     'aaabcd' -> False
     '' -> True      (empty string)
-
     :param some_str:
     :return: bool
     """
-    return None
+    Set = list(set(some_str))
+    List = list(some_str)
+
+    Set.sort()
+    List.sort()
+
+    if Set == List:
+        return True
+    return False
 
 
 def list_diff(elements):
     """
     1 Kata
-
     Given a list of integers as an input, return the "diff" list - each element is
     reduces by its previous one. The first element should be None
-
     e.g.
     [1, 2, 3, 4, 7, 11] -> [None, 1, 1, 1, 3, 4]
     [] -> []
     [1, 5, 0, 4, 1, 1, 1] -> [None, 4, -5, 4, -3, 0, 0]
-
     :param elements: list of integers
     :return: the diff list
     """
-    return None
+    diff_List = [[None], [elements[i+1] - elements[i] for i in range(len(elements)-1)]]
 
+    diff_List = sum(diff_List,[])
+
+    return diff_List
 
 def prime_number(num):
     """
     1 Kata
-
     Check if the given number is prime or not.
-
     hint: use the built-in function "range"
     :param num: the number to check
     :return: bool. True if prime, else False
     """
-    return None
+    if isinstance(num, int):
+        if num <= 1:
+            return False
+        for i in range(2, num):
+            if num%i == 0:
+                return False
+        return True
+    return False
 
 
 def palindrome_num(num):
     """
     1 Kata
-
     Check whether a number is palindrome or not
-
     e.g.
     1441 -> True
     123 -> False
-
     :param num: int
     :return: bool. True is palindrome, else False
     """
-    return None
+    if str(num) == str(num)[::-1]:
+
+        return True
+
+    return False
 
 
 def pair_match(men, women):
     """
     3 Kata
-
     This function gets two dictionaries of the type:
     {
         "<name>": <age>
     }
-
     Where <name> is a string name, and <age> is an integer representing the age
     The function returns a pair of names (tuple), of from men dict, the other from women dict,
     where their absolute age differences is the minimal
-
     e.g.
     men = {"John": 20, "Abraham": 45}
     women = {"July": 18, "Kim": 26}
-
     The returned value should be a tuple ("John", "July") since:
-
     abs(John - Kim) = abs(20 - 26) = abs(-6) = 6
     abs(John - July) = abs(20 - 18) = abs(2) = 2
     abs(Abraham - Kim) = abs(45 - 26) = abs(19) = 19
     abs(Abraham - July) = abs(45 - 18) = abs(27) = 27
-
     :param men: dict mapping name -> age
     :param women: dict mapping name -> age
     :return: tuple (men_name, women_name) such their age absolute difference is the minimal
     """
-    return None
+    minAgeD = float('inf')
+    closeMatch = None
+    for man in men:
+        for woman in women:
+            ageDiff = abs(men[man] - women[woman])
+            if ageDiff < minAgeD:
+                minAgeD = ageDiff
+                closeMatch = (man, woman)
+    return closeMatch
 
 
 def bad_average(a, b, c):
     """
     1 Kata
-
     This function gets 3 numbers and calculates the average.
     There is a mistake in the following implementation, you are required to fix it
-
     :return:
     """
-    return a + b + c / 3
+    return (a + b + c) / 3
 
 
 def best_student(grades):
     """
     1 Kata
-
     This function gets a dict of students -> grades mapping, and returns the student with the highest grade
-
     e.g.
     {
         "Ben": 78,
@@ -182,19 +189,18 @@ def best_student(grades):
         "Efraim": 65,
         "Rachel": 95
     }
-
     will return "Natan"
-
     :param grades: dict of name -> grade mapping
     :return: str. some key from the dict
     """
-    return None
+    for i in grades:
+        return list(grades.keys())[list(grades.values()).index(max(grades.values()))]
+
 
 
 def print_dict_as_table(some_dict):
     """
     1 Kata
-
     Prints dictionary keys and values as the following format. For:
     {
         "Ben": 78,
@@ -203,9 +209,7 @@ def print_dict_as_table(some_dict):
         "Efraim": 65,
         "Rachel": 95
     }
-
     The output will be:
-
     Key     Value
     -------------
     Ben     78
@@ -213,79 +217,101 @@ def print_dict_as_table(some_dict):
     Natan   99
     Efraim  65
     Rachel  95
-
     :param some_dict:
     :return:
     """
-    return None
-
+    print("Key      Value")
+    print("--------------")
+    newString = ""
+    for key, value in some_dict.items():
+            newString = newString + ("{:<10} {:<10}\n".format(key, value))
+    return newString
 
 def merge_dicts(dict1, dict2):
     """
     1 Kata
-
     This functions merges dict2's keys and values into dict1, and returns dict1
-
     e.g.
     dict1 = {'a': 1}
     dict2 = {'b': 2}
-
     The results will by
     dict1 = {'a': 1, 'b': 2}
-
     :param dict1:
     :param dict2:
     :return:
     """
+    for a, value in dict2.items():
+        dict1[a] = value
     return dict1
+
 
 
 def seven_boom(n):
     """
     1 Kata
-
     This functions returns a list of all "Booms" for a 7-boom play starting from 1 to n
-
     e.g. For n = 30
     The return value will be [7, 14, 17, 21, 27, 28]
-
     :param n: int. The last number for count for a 7-boom play
     :return: list of integers
     """
-    return None
+    if n < 7:
+        return []
+    
+    boom_List = []
+    
+    for num in range(7, n + 1):
+        if "7" in str(num) or num % 7 == 0:
+            boom_List.append(num)
+    
+    return boom_List
+
+
 
 
 def caesar_cipher(str_to_encrypt):
     """
     2 Kata
-
     This function encrypts the given string according to caesar cipher (a - d, b - e, ..., y - b, z - c etc...).
     Spaces remain as they are. You can assume the string contain a-z and A-Z chars only.
-
     e.g.
     Fly Me To The Moon -> Iob Ph Wr Wkh Prrq
-
     :return:
     """
-    return None
+    enc = 3
+
+    answer = ""
+    
+    for i in range(len(str_to_encrypt)):
+        char = str_to_encrypt[i]
+
+        if char == " ":
+            answer += " "
+        
+        elif (char.isupper()):
+            answer += chr((ord(char) + enc - 65) % 26 + 65)
+        
+        else:
+            answer += chr((ord(char) + enc - 97) % 26 + 97)
+    
+    return answer
 
 
 def sum_of_digits(digits_str):
     """
     1 Kata
-
     Calculates the sum of digits in a string (you can assume the input is a string containing numeric digits only)
-
     e.g.
     '2524' -> 13
     '' -> 0
     '00232' -> 7
-
-
     :param digits_str: str of numerical digits only
     :return: int representing the sum of digits
     """
-    return None
+    sum = 0
+    for i in digits_str:
+        sum += int(i)
+    return sum
 
 
 if __name__ == '__main__':
@@ -312,12 +338,20 @@ if __name__ == '__main__':
     print(list_diff([1, 2, 3, 8, 77, 0]))
 
     print('\nprime_number:\n--------------------')
+    print(prime_number(2))
     print(prime_number(5))
     print(prime_number(22))
+    print(prime_number(5))
+    print(prime_number(11))
+    print(prime_number(12))
+    print(prime_number(7))
 
     print('\npalindrome_num:\n--------------------')
     print(palindrome_num(12221))
     print(palindrome_num(577))
+    print(palindrome_num(5775))
+    print(palindrome_num(123456))
+    print(palindrome_num(123456654321))
 
     print('\npair_match:\n--------------------')
     print(pair_match(
@@ -363,4 +397,3 @@ if __name__ == '__main__':
 
     print('\nsum_of_digits:\n--------------------')
     print(sum_of_digits('1223432'))
-
