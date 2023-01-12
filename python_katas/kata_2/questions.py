3cbd08a7bc15927cc8402455619c473944bcdf83
Rosin1992 <114486997+Rosin1992@users.noreply.github.com>
2023-01-12 20:00:50 +0200

    Update questions.py

--- python_katas/kata_2/questions.py
+++ python_katas/kata_2/questions.py
@@ -1,14 +1,11 @@
 def valid_parentheses(s):
     """
     3 Kata
-
     This function gets a string containing just the characters '(', ')', '{', '}', '[' and ']',
     and determines if the input string is valid.
-
     An input string is valid if:
         Open brackets must be closed by the same type of brackets.
         Open brackets must be closed in the correct order.
-
     e.g.
     s = '[[{()}](){}]'  -> True
     s = ']}'          -> False
@@ -44,230 +41,309 @@ def valid_parentheses(s):
 def fibonacci_fixme(n):
     """
     2 Kata
-
     A Fibonacci sequence is the integer sequence of 1, 1, 2, 3, 5, 8, 13....
     The first two terms are 1 and 1. All other terms are obtained by adding the preceding two terms.
-
     This function should return the n'th element of fibonacci sequence. As following:
-
     fibonacci_fixme(1) -> 1
     fibonacci_fixme(2) -> 1
     fibonacci_fixme(3) -> 2
     fibonacci_fixme(4) -> 3
     fibonacci_fixme(5) -> 5
-
     But it doesn't (it has some bad lines in it...)
     You should (1) correct the for statement and (2) swap two lines, so that the correct fibonacci element will be returned
     """
     a = 0
     b = 1
     for i in range(1, n):
-        a = b
         tmp = a + b
+        a = b
         b = tmp
-
-    return a
+    return b
 
 
 def most_frequent_name(file_path):
     """
     2 Kata
-
     This function gets a path to a file containing names (name in each line)
     The function should return the most frequent name in the file
-
     You can assume file_path exists in the file system
-
     :param file_path: str - absolute or relative file to read names from
     :return: str - the mose frequent name. If there are many, return one of them
     """
-    return None
+    nameF = {}
+
+    with open(file_path, 'r') as file:
+        for L in file:
+            L = L.strip()
+
+            if L in nameF:
+                nameF[L] += 1
+
+            else:
+                nameF[L] = 1
+
+    most_f_name = None
+
+    highest_frequency = 0
+
+    for name, freq in nameF.items():
+
+        if freq > highest_frequency:
+            most_f_name = name
+            highest_frequency = freq
+
+    return most_f_name
+
+
+
+
 
 
 def files_backup(dir_path):
     """
     3 Kata
-
     This function gets a path to a directory and generated a .gz file containing all the files the directory contains
     The backup .gz file name should be in the form:
-
     'backup_<dir_name>_<yyyy-mm-dd>.tar.gz'
-
     Where <dir_name> is the directory name (only the directory, not the full path given in dir_path)
     and <yyyy-mm-dd> is the date e.g. 2022-04-10
-
     You can assume dir_path exists in the file system
-
     :param dir_path: string - path to a directory
     :return: str - the backup file name
     """
-    return None
+    import os
+    import time
+    import tarfile
+
+    dir_n = os.path.basename(dir_path)
+
+    current_date = time.strftime("%Y-%m-%d")
+
+    backup_file_name = f"backup_{dir_n}_{current_date}.tar.gz"
+
+    with tarfile.open(backup_file_name, "w:gz") as tar:
+        tar.add(dir_path, arcname=dir_n)
 
 
 def replace_in_file(file_path, text, replace_text):
     """
     2 Kata
-
     This function gets a path of text file, it replaces all occurrences of 'text' by 'replace_text'.
     The function saves the replaces content on the same path (overwrites the file's content)
-
     You MUST check that file_path exists in the file system before you try to open it
-
     :param file_path: relative or absolute path to a text file
     :param text: text to search
     :param replace_text: text to replace with
     :return: None
     """
-    return None
+    import os
+    if not os.path.exists(file_path):
+        raise ValueError("File does not exist: {}".format(file_path))
+
+    with open(file_path, 'r') as f:
+        cont = f.read()
+
+    cont = cont.replace(text, replace_text)
+
+    with open(file_path, 'w') as f:
+        f.write(cont)
 
 
 def json_configs_merge(*json_paths):
     """
     2 Kata
-
     This function gets an unknown number of paths to json files (represented as tuple in json_paths argument)
     it reads the files content as a dictionary, and merges all of them into a single dictionary,
     in the same order the files have been sent to the function!
-
     :param json_paths:
     :return: dict - the merges json files
     """
-    return None
+    import json
+    m_dict = {}
+    for path in json_paths:
+      with open(path, 'r') as f:
+        Fdict = json.load(f)
+        m_dict.update(Fdict)
+    return m_dict
 
 
 def monotonic_array(lst):
     """
     1 Kata
-
     This function returns True/False if the given list is monotonically increased or decreased
-
     :param lst: list of numbers (int, floats)
     :return: bool: indicating for monotonicity
     """
-    return None
-
+    if len(lst) < 2:
+        return True
+    elif lst[0] < lst[1]:
+        return all(x < y for x, y in zip(lst, lst[1:]))
+    else:
+        return all(x > y for x, y in zip(lst, lst[1:]))
 
 def matrix_avg(mat, rows=None):
     """
     2 Kata
-
     This function gets a 3*3 matrix (list of 3 lists) and returns the average of all elements
     The 'rows' optional argument (with None as default) indicating which rows should be included in the average calculation
-
     :param mat: 3*3 matrix
     :param rows: list of unique integers in the range [0, 2] and length of maximum 3
     :return: int - the average values
     """
-    return None
+    if rows is None:
+        rows = range(3)
+
+    all = 0
+    cnt = 0
+
+    for i in rows:
+
+        for j in range(3):
+            all += mat[i][j]
+            cnt += 1
+
+    return all / cnt
 
 
 def merge_sorted_lists(l1, l2):
     """
     1 Kata
-
     This function gets two sorted lists (each one of them is sorted)
     and returns a single sorted list combining both of them.
-
     Try to be as efficient as you can (hint - don't use Python's built in sort() or sorted() functions)
-
     :param l1: list of integers
     :param l2: list of integers
     :return: list: sorted list combining l1 and l2
     """
-    return None
+    m_s_list = []
+    i = 0
+    j = 0
+    while i < len(l1) and j < len(l2):
+
+        if l1[i] < l2[j]:
+            m_s_list.append(l1[i])
+            i += 1
+
+        else:
+            m_s_list.append(l2[j])
+            j += 1
+
+    m_s_list += l1[i:]
+    m_s_list += l2[j:]
+
+    return m_s_list
 
 
 def longest_common_substring(str1, str2):
     """
     4 Kata
-
     This functions gets two strings and returns their longest common substring
-
     e.g. for
     str1 = 'Introduced in 1991, The Linux kernel is an amazing software'
     str2 = 'The Linux kernel is a mostly free and open-source, monolithic, modular, multitasking, Unix-like operating system kernel.'
-
     The returned value would be 'The Linux kernel is a'
     since it's the longest string contained both in str1 and str2
-
     :param str1: str
     :param str2: str
     :return: str - the longest common substring
     """
-    return None
+    longest_common_substring = ""
+
+    for i, ch_1 in enumerate(str1):
+
+        for j, ch_2 in enumerate(str2):
+
+            if ch_1 == ch_2:
+                temp_common_substring = ch_1
+                temp1, temp2 = i, j
+
+                while temp1 < len(str1) and temp2 < len(str2) and str1[temp1] == str2[temp2]:
+                    temp_common_substring += str1[temp1]
+                    temp1 += 1
+                    temp2 += 1
+
+                if len(temp_common_substring) > len(longest_common_substring):
+                    longest_common_substring = temp_common_substring
+
+    return longest_common_substring
 
 
 def longest_common_prefix(str1, str2):
     """
     1 Kata
-
     This functions gets two strings and returns their longest common prefix
-
     e.g. for
     str1 = 'The Linux kernel is an amazing software'
     str2 = 'The Linux kernel is a mostly free and open-source, monolithic, modular, multitasking, Unix-like operating system kernel.'
-
     The returned value would be 'The Linux kernel is a'
-
     :param str1: str
     :param str2: str
     :return: str - the longest common prefix
     """
-    return None
+    minL = min(len(str1), len(str2))
+    Cprefix = ""
+    for i in range(minL):
+        if str1[i] == str2[i]:
+            Cprefix += str1[i]
+        else:
+            return Cprefix
+    return Cprefix
+
 
 
 def rotate_matrix(mat):
     """
     2 Kata
-
     This function gets a matrix n*m (list of m lists of length n) and rotate the matrix clockwise
     e.g.
     for [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]] which represent the matrix
-
     1   2   3
     4   5   6
     7   8   9
     10  11  12
-
     The output should be:
     [[10, 7, 4, 1], [11, 8, 5, 2], [12, 9, 6, 3]]
-
     10  7   4   1
     11  8   5   2
     12  9   6   3
-
     :param mat:
     :return: list of lists - rotate matrix
     """
-    return None
+    A = len(mat[0])
+    rotmatx = []
+    for i in range(A):
+        rotmatx.append([row[i] for row in mat[::-1]])
+    return
 
 
 def is_valid_email(mail_str):
     """
     3 Kata
-
     This function returns True if the given mail is in the form:
     (username)@(domainname)
-
     Where
     * (username) must start with digit or an English character, and can contains only 0-9 a-z A-Z . or _
     * (domainname) is a real, existed domain - one that resolves to an actual ip address
-
     Hint: use socket.gethostbyname() to resolve a DNS in Python code
-
     :param mail_str: mail to check
     :return: bool: True if it's a valid mail (otherwise either False is returned or the program can crash)
     """
-    return None
+    import socket
+    import re
 
-
+    if not re.match(r'^[a-zA-Z0-9]+(\.[a-zA-Z0-9_]+)*@[0-9a-zA-Z_]*\.[a-zA-Z]*$', mail_str):
+        return False
+    username, domainName = mail_str.split('@')
+    try:
+        socket.gethostbyname(domainName)
+        return True
+    except:
+        return False
 def pascal_triangle(lines):
     """
     3 Kata
-
     This function gets an integer representing the number of lines to print in a Pascal Triangle
     e.g. For n = 10 then following would be printed
-
                  1
                 1 1
                1 2 1
@@ -278,7 +354,6 @@ def pascal_triangle(lines):
         1 7 21 35 35 21 7 1
       1 8 28 56 70 56 28 8 1
     1 9 36 84 126 126 84 36 9 1
-
     You are allowed to print the numbers not in a triangle shape:
     1
     1 1
@@ -290,44 +365,73 @@ def pascal_triangle(lines):
     1 7 21 35 35 21 7 1
     1 8 28 56 70 56 28 8 1
     1 9 36 84 126 126 84 36 9 1
-
     :param lines: int
     :return: None
     """
-    return None
+
+    tr = []
+
+    for i in range(lines):
+
+        if i == 0:
+            tr.append([1])
+
+        else:
+
+            row = []
+
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    row.append(1)
+                else:
+                    row.append(tr[i - 1][j - 1] + tr[i - 1][j])
+
+            tr.append(row)
+
+    trStr = ""
+
+    for row in tr:
+        row_str = " ".join(str(x) for x in row)
+        trStr += row_str + "\n"
+
+    return trStr
 
 
 def list_flatten(lst):
     """
     2 Kata
-
     This function gets a list of combination of integers or nested lists
     e.g.
     [1, [], [1, 2, [4, 0, [5], 6], [5, 4], 34, 0], [3]]
-
     The functions should return a flatten list (including all nested lists):
     [1, 1, 2, 4, 0, 5, 6, 5, 4, 34, 0, 3]
-
     :param lst: list of integers of another list
     :return: flatten list
     """
-    return None
+    flatten_list = []
+
+    for i in lst:
+
+        if isinstance(i, list):
+            flatten_list.extend(list_flatten(i))
+
+        else:
+            flatten_list.append(i)
+
+    return flatten_list
+
 
 
 def str_compression(text):
     """
     2 Kata
-
     This function gets a text (string) and returns a list representing the compressed form of the text.
     e.g.
     text = 'aaaaabbcaasbbgvccf'
-
     The output will be:
     ['a', 5, 'b', 2, 'c', 'a', 2, 's', 1, 'b', 2, 'g', 'v', 'c', 2, 'f']
-
     Since 'a' appears 5 times in consecutively, 'b' 2 times etc...
     Note that sequences of length 1 don't necessarily have the number 1 after the character (like 'c' before 'a')
-
     :param text: str
     :return: list representing the compressed form of the string
     """
@@ -337,14 +441,12 @@ def str_compression(text):
 def strong_pass(password):
     """
     1 Kata
-
     A password is considered strong if it satisfies the following criteria:
     1) Its length is at least 6.
     2) It contains at least one digit.
     3) It contains at least one lowercase English character.
     4) It contains at least one uppercase English character.
     5) It contains at least one special character. The special characters are: !@#$%^&*()-+
-
     This function returns True if the given password is strong enough
     """
     return None
